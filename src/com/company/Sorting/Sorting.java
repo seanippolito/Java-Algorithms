4f4a05246e0bdc7c6126ee44397a05f3ff159345
Sean Ippolito <ippolitosean@gmail.com>
2017-10-09 22:10:46 -0700

    The struggle is real

--- src/com/company/Sorting/Sorting.java
+++ src/com/company/Sorting/Sorting.java
@@ -5,26 +5,52 @@ import java.util.*;
 
 public class Sorting {
     private static Random random = new Random();
+    private static void swap(int[] a, int l, int r) {
+        int temp = a[l];
+        a[l] = a[r];
+        a[r] = temp;
+    }
 
     private static int[] partition3(int[] a, int l, int r) {
-      //write your code here
-
-
-      int m1 = l;
-      int m2 = r;
-      int[] m = {m1, m2};
-      return m;
+        //write your code here
+        int x = a[l];
+        int m1 = l;
+        int m2 = r;
+        int[] m = {m1, m2};
+        for(int i = 0; i < r; i ++) {
+            System.out.println(i + "th iteration");
+            System.out.println(x + " = x value");
+            System.out.println(a[i] + " = a[i]");
+            if (a[i] < x ) {
+                m1++;
+                System.out.println(m1 + " = m1");
+                System.out.println(a[m1] + " = a[m1]");
+                swap(a, i, m1);
+                System.out.println("after swap a[i] = " + a[i] + " a[m1] = " + a[m1]);
+                m2++;
+                swap(a, m1, m2);
+            }else if (a[i] > x) {
+                m2--;
+                System.out.println(m2 + " = m2");
+                System.out.println(a[m2] + " = m2");
+                swap(a, i, m2);
+                System.out.println("after swap a[i] = " + a[i] + " a[m2] = " + a[m2]);
+                //i--;
+            }
+        }
+        swap(a, l, m1);
+        m[0] = m1;
+        m[1] = m2;
+        return m;
     }
 
     private static int partition2(int[] a, int l, int r) {
         int x = a[l];
         int j = l;
         for (int i = l + 1; i <= r; i++) {
-            if (a[i] <= x) {
+            if (a[i] < x) {
                 j++;
-                int t = a[i];
-                a[i] = a[j];
-                a[j] = t;
+                swap(a, i, j);
             }
         }
         int t = a[l];
@@ -42,9 +68,9 @@ public class Sorting {
         a[l] = a[k];
         a[k] = t;
         //use partition3
-        int m = partition2(a, l, r);
-        randomizedQuickSort(a, l, m - 1);
-        randomizedQuickSort(a, m + 1, r);
+        int[] m = partition3(a, l, r);
+        randomizedQuickSort(a, l, m[0] - 1);
+        randomizedQuickSort(a, m[1]+1, r);
     }
 
     public static void main(String[] args) {
